8bf26f77d2dcca46e5aabcf4819eae320301fae5
Kinori <russel.richard@outlook.com>
2023-01-15 16:16:31 +0000

    refactored js code

--- script.js
+++ script.js
@@ -89,11 +89,11 @@ var upperCasedCharacters = [
 ];
 
 // Prompt the user to select a password length between 10 and 64
-//Validate that the correct length was selected
+// Validate that the correct length was selected
 // Prompt the user to decide on upper case characters
 // Prompt the user to decide on lower case characters
 // Prompt the user to decide on special characters
-//Ensure that at least one character type is selected
+// Ensure that at least one character type is selected
 // Collect user selections into an empty array
 // Display the randomly generated password in an alert or written on the page
 
@@ -108,27 +108,28 @@ function getPasswordOptions() {
   // Use a while loop to validate that the user input for password length is a number between 10 and 64 characters)
   while (passwordLength < 10 || passwordLength > 64 || isNaN(passwordLength)) {
 
-    passwordLength = prompt("How long would you like the password? Enter a number between 10 and 64.");     //Prompt the user to choose a password length
+    passwordLength = prompt("How long would you like the password? Enter a number between 10 and 64.");     // Prompt the user to choose a password length
 
-    //Use an if statement to verify that the user makes the right selection
+    // Use an if statement to verify that the user makes the right selection
     if (passwordLength < 10 || passwordLength > 64 || isNaN(passwordLength)) {
-      alert("Please enter a number between 10 and 64 characters.");      //Alert the user to input their selection again if they made a wrong selection
+      alert("Please enter a number between 10 and 64 characters.");      // Alert the user to input their selection again if they made a wrong selection
     }
+    
   }
-  if (confirm("Would you like any upper case characters?")) {       //Prompt the user to accept or reject upper case characters
-    characterSet = characterSet.concat(upperCasedCharacters);     //Place the upper case characters in the characterSet array, if accepted
-  }
-  if (confirm("Would you like any lower case characters?")) {       //Prompt the user to accept or reject lower case characters
-    characterSet = characterSet.concat(lowerCasedCharacters);     //Place the lower case characters in the characterSet array, if accepted
-  }
-  if (confirm("Would you like any special characters?")) {          //Prompt the user to accept or reject special characters
-    characterSet = characterSet.concat(specialCharacters);        //Place the special characters in the characterSet array, if accepted
-  }
-  if (confirm("Would you like any numbers?")) {                     //Prompt the user to accept or reject numbers characters
-    characterSet = characterSet.concat(numericCharacters);        //Place the numeric characters in the characterSet array, if accepted
+  if (confirm("Would you like any upper case characters?")) {     // Prompt the user to accept or reject upper case characters
+    characterSet = characterSet.concat(upperCasedCharacters);     // Place the upper case characters in the characterSet array, if accepted
+  } 
+  if (confirm("Would you like any lower case characters?")) {     // Prompt the user to accept or reject lower case characters
+    characterSet = characterSet.concat(lowerCasedCharacters);     // Place the lower case characters in the characterSet array, if accepted
+  } 
+  if (confirm("Would you like any special characters?")) {        // Prompt the user to accept or reject special characters
+    characterSet = characterSet.concat(specialCharacters);        // Place the special characters in the characterSet array, if accepted
+  } 
+  if (confirm("Would you like any numbers?")) {                   // Prompt the user to accept or reject numbers characters
+    characterSet = characterSet.concat(numericCharacters);        // Place the numeric characters in the characterSet array, if accepted
   }
 
-  return characterSet;      //end the function and return the values of characterSet array
+  return characterSet;      // End the function and return the values of characterSet array
 
 };
 
@@ -138,12 +139,12 @@ function getRandom() {
 
   let newPassword = "";   // Create an empty variable to hold the newly generaed password
 
-  //Create a for loop that iterates though the the amount of characters selected for the pasword length 
+  // Create a for loop that iterates though the the amount of characters selected for the pasword length 
   for (let i = 0; i < passwordLength; i++) {
     newPassword += characterSet[Math.floor(Math.random() * characterSet.length)]    //Randomise the amount characters selected and round down to the nearest number
   };
 
-  return newPassword;       //end the function and return the value of the newly generated password
+  return newPassword;       // End the function and return the value of the newly generated password
 
 };
 
@@ -155,10 +156,9 @@ function generatePassword() {
 
   let password = getRandom()     // Call the function that randomises the characters and generates a new password
 
-  return password;      //display the newly generated password
+  return password;      // Display the newly generated password
 
 }
-
 
 
 // Get references to the #generate element
